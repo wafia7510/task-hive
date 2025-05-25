0a7e16ac6e214ce9f153005eb8bff4c29ecaf786
wafia7510 <wafiadr2@gmail.com>
2025-05-25 15:51:00 +0500

    Fix: API base URL and 415 content type

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+
 export const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL,
   withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
